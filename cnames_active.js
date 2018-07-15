4d6609bdb15edb3ee85da182332b1d736eb2414c
xvinc <7457568+xvinc@users.noreply.github.com>
2018-07-15 20:31:39 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1370,6 +1370,7 @@ var cnames_active = {
   "xprmntl": "xprmntl.github.io", // noCF? (don´t add this in a new PR)
   "xto6": "mohebifar.github.io/xto6", // noCF? (don´t add this in a new PR)
   "xtype": "lucono.github.io/xtypejs", // noCF? (don´t add this in a new PR)
+  "xvinc":"xvinc.github.io",
   "y86": "quietshu.github.io/y86", // noCF? (don´t add this in a new PR)
   "yadl": "yadljs.github.io",
   "yagolopez": "yagolopez.github.io",
